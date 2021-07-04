90095244d1cc646b2bb5976b0cfa2409fb813987
Rekha Kumari <85360674+rekha25k@users.noreply.github.com>
2021-07-04 18:43:31 +0000

    style: Added chalk

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var readLineSync = require("readline-sync");
 const chalk = require("chalk");
 var score = 0;
 
-console.log("Welcome to DO YOU KNOW Rekha Quiz!\n");
+function welcomeMessage(){
+console.log("Welcome to "+chalk.italic.bgCyan("DO YOU KNOW Rekha ")+ "Quiz!\n");
 var userName = readLineSync.question("Please enter your name ");
-console.log("Hi "+userName+ ". Take the quiz below to find out how well you know Rekha :D");
+
+console.log(chalk.green("Hi "+userName+ ". Take the quiz below to find out how well you know Rekha :D"));
+
+console.log(chalk.bgMagenta("\nNote:- For every right answer, your will be getting +1 points\nGood luck!\n"))
+  
+
+}
 
 var questionBank = [
   {
@@ -13,22 +20,22 @@ var questionBank = [
   },
 
   {
-    question:"Which is my favorite restaurant?",
+    question:"Which is my favorite restaurant? ",
     answer: "Peter Cat"
   },
 
   {
-    question:"Which word do I say way too often?",
+    question:"Which word do I say way too often? ",
     answer: "like"
   },
 
   {
-    question:"Am I a dog or a cat person?",
+    question:"Am I a dog or a cat person? ",
     answer: "cat"
   },
 
   {
-    question:"What do I prefer tea or coffee",
+    question:"What do I prefer tea or coffee? ",
     answer: "coffee"
   },
   
@@ -45,31 +52,52 @@ var highScores = [
   }
 ]
 
+function greetingMessage(){
+  var currentTime = new Date();
+  var currentOffset = currentTime.getTimezoneOffset();
+
+  var ISTOffset = 330;   // IST offset UTC +5:30 
+  var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+  // ISTTime now represents the time in IST coordinates
+
+  var hours = ISTTime.getHours()
+
+  if (hours < 12 && hours >4) {
+    console.log(chalk.bold("Good morning!"));
+  }else if(hours > 12 && hours< 18) {
+    console.log(chalk.bold("Good afternoon!"));
+  }else{
+    console.log(chalk.bold("Good evening!"));
+  }
+}
+
 
   function play(){
 for(let i=0; i < questionBank.length ; i++){
-  console.log("Question "+(i+1));
+  console.log(chalk.bgYellow("Question "+(i+1)));
   var currentQuestion = questionBank[i];
 quiz(currentQuestion);
 }
 console.log("Thanks for playing!");
-console.log("The Highest Scores");
+console.log(chalk.bgMagenta("\nThe Highest Scores\n"));
 
-highScores.map(score => console.log(chalk.bgMagenta(score.name)+ " : "+ chalk.inverse(score.score)));
+highScores.map(score => console.log(chalk.bgBlue(score.name)+ " : "+ chalk.inverse(score.score)));
   
-console.log("If your name should be here, dm me on Insta @rekha_25, I'll add your name to the list :)");  
+console.log(chalk.bgCyan("\nIf your name should be here, dm me on Insta @rekha_25, I'll add your name to the highest scorers list :)"));  
   }
 function quiz(currentQuestion){
   
   var userAns =  readLineSync.question(currentQuestion.question);
   if(userAns.toLowerCase() === currentQuestion.answer.toLowerCase()){
-    console.log("You are right!");
+    console.log(chalk.green("You are right!"));
     score++;
   }
   else{
-  console.log("You are wrong!"); }
+  console.log(chalk.red("You are wrong!")); }
   console.log("Your current score is " +score);
   console.log("-----------------------------");
 }
 
+greetingMessage();
+welcomeMessage();
 play();
